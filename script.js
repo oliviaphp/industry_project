e3bf50cc1dd29ff64feb61a229335276fe32f8b5
oliviaphp <oliviaphp95@gmail.com>
2018-08-08 11:56:58 +0100

    Speech to text added and images fixed

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 
+// Speech to text
+// speech-to-text algorihtm adapted from https://github.com/wesbos/JavaScript30/blob/master/20%20-%20Speech%20Detection/index-FINISHED.html
+
+var dictation; // stores the word that we want to pass to the biomedical extractor
+
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  recognition.lang = 'en-US';
+  
+  let p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+
+      if (e.results[0].isFinal) {
+        var text = transcript;
+        //console.log(text);
+        dictation = encodeURIComponent(text.trim()); //replaces spaces with %20 to become a URL
+        //console.log(dictation);
+        biomedicalExtractor(dictation); // passes the text in URL format to biomedicalExtractor function
+
+       }
+  });
+  recognition.addEventListener('end', recognition.start); // this means the app will conitnue to listen even if there is a pause
+  recognition.start();
+
+
+
 var originalArray = new Array(); // stores medical entities 
 
 function nextWord() {
@@ -8,8 +42,9 @@ function nextWord() {
         if(originalArray[i] == document.getElementById("tokenInput").innerHTML) {
             if(originalArray[i] != originalArray[originalArray.length - 1]) {
                     var next = originalArray[i+1]; // gets the next word in the array
-                    console.log(next);
+                    //console.log(next);
                     getWiki(next);
+
                     break;
                 }
                 else {
@@ -27,7 +62,7 @@ function previousWord() {
             if(originalArray[i] == document.getElementById("tokenInput").innerHTML) {
                 if(originalArray[i] != originalArray[0]) {
                     var prev = originalArray[i-1]; // gets the previous word in the array
-                    console.log(prev);
+                    //console.log(prev);
                     getWiki(prev);
                 }
                 else {
@@ -43,10 +78,14 @@ function previousWord() {
 
 }
 
+
+function biomedicalExtractor(dictation) {
+
 // retrives biomedical words from cTAKES
 //$(document).ready(function(){
     $.ajax({
-        url: "http://localhost:9999/ctakes?text=I%20would%20suggest%20taking%20paracetamol%20for%20your%20headache%20and%20possibly%20aspirin%20or%20codeine",
+        //url: "http://localhost:9999/ctakes?text=I%20would%20suggest%20taking%20paracetamol%20for%20your%20headache%20and%20possibly%20aspirin%20or%20codeine",
+        url: "http://localhost:9999/ctakes?text=" + dictation,
         dataType: 'json',
         success: function(data) {
         var jcontent = data; // assigns the data from the JSON file to the variable jcontent
@@ -59,13 +98,16 @@ function previousWord() {
                  $(next).each(function(index, value){ // this will ensure we return the correct word
                     if(value.typ == "org.apache.ctakes.typesystem.type.textsem.MedicationMention") {
                         $(current).each(function(key, value) {
-                            $.each(value.annotation , function(key, value ){  
+                            $.each(value.annotation , function(key, value){  
                                 if(key == "canonicalForm") {
-                                    // token = value;
+                                    // ensures no duplicates in array
+                                    originalArray.indexOf(value) === -1 ? originalArray.push(value) : console.log("This item already exists");
                                     // document.getElementById("tokenInput").innerHTML = token;
-                                    // getWiki(token); //passes the token value to getWiki to retrieve the info
-                                    originalArray.push(value);
-                                }
+                                    getWiki(value); //passes the token value to getWiki to retrieve the info
+                                    console.log(originalArray);
+
+                                    
+                            }
 
                             }); 
                             
@@ -80,33 +122,37 @@ function previousWord() {
 
                 }
              });
-        getWiki(originalArray[0]);
+       //getWiki(originalArray[0]);
     }
 });
     
 //});
+}
 
 
 
 // retrives text information about word
 function getWiki(token) {
-    document.getElementById("tokenInput").innerHTML = token;
 
         // recieves article summary at top of Wiki page
         var URL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext&=';
 
         //var place = document.getElementById('userinput').value;
-        var place = token;
-        URL += "&titles=" + place;
+        //var place = token;
+        URL += "&titles=" + token;
         URL += "&rvprop=content";
         URL += "&callback=?";
         //console.log(URL);
         $.getJSON(URL, function (data) {
             var obj = data.query.pages;
             var ob = Object.keys(obj)[0];
-            //console.log(obj[ob]["extract"]);
+            //console.log(obj[ob]["extract"]);  
+
+            document.getElementById("tokenInput").innerHTML = token;
             document.getElementById("wiki_intro").innerHTML = obj[ob]["extract"];
+        
             imageWiki(token);
+
 
         });
 
@@ -134,10 +180,8 @@ function setButtonColor(color) {
 }
 
 // retrives images
-    function imageWiki(token) {
+function imageWiki(token) {
     $("#img").html(""); // clears contents from earlier searches
-    //var word = document.getElementById('tokenInput').value;
-    console.log(token);
 
     // $.ajaxPrefilter(function (options) {
     //     if (options.crossDomain && jQuery.support.cors) {
@@ -148,8 +192,7 @@ function setButtonColor(color) {
 
     $.get(
         'https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=' + token + '&callback=?',
-        //'https://en.wikipedia.org/w/api.php?action=query&titles=' + token + '&prop=images', // na
-        
+        //'https://en.wikipedia.org/w/api.php?action=query&titles=' + token + '&prop=images', // na        
 
     function (response) {
         var m;
@@ -165,5 +208,25 @@ function setButtonColor(color) {
             $("#img").append('<img src="' + window.location.protocol + url + '">');
         });
     });
+
+    //console.log('https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=' + token + '&callback=?');
+}
+
+// exports the session into an XML/JSON format
+function exportSession() {
+    if(originalArray.length == 0) {
+        alert("Nothing to export");
+    }
+    else {
+        alert("Session exported");
+        var fs = require("fs");
+        fs.writeFile("./object.json", JSON.stringify(originalArray, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+            console.log("File has been created");
+        });
+    }
 }
 
